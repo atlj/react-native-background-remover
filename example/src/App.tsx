a57e575861744a23947563825d3f031432942448
Burak Güner <burak.guner@callstack.com>
2024-02-04 17:50:37 +0300

    chore(example): use image picker on the main screen

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,57 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-background-remover';
+import { StyleSheet, View } from 'react-native';
+import { launchImageLibrary } from 'react-native-image-picker';
+import { Button } from 'react-native';
+import { Image } from 'react-native';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [imageURI, setImageURI] = React.useState<string | null>(null);
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  async function selectImage() {
+    const imagePickerResponse = await launchImageLibrary({
+      mediaType: 'photo',
+      selectionLimit: 1,
+      presentationStyle: 'formSheet',
+    });
+
+    const selectedImage = imagePickerResponse.assets?.[0];
+
+    if (!selectedImage || !selectedImage.uri) {
+      return;
+    }
+
+    setImageURI(selectedImage.uri);
+  }
+
+  function clearSelection() {
+    setImageURI(null);
+  }
+
+  function removeSelectionBackground() {
+    // TODO: Implement background removal
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      {imageURI ? (
+        <View style={styles.container}>
+          <Image source={{ uri: imageURI }} style={styles.image} />
+          <View style={styles.buttonContainer}>
+            <Button
+              title="Clear Selection"
+              onPress={clearSelection}
+              color="red"
+            />
+            <Button
+              title="Remove Background"
+              onPress={removeSelectionBackground}
+            />
+          </View>
+        </View>
+      ) : (
+        <Button title="Open Image Library" onPress={selectImage} />
+      )}
     </View>
   );
 }
@@ -22,10 +61,14 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 36,
+  },
+  image: {
+    width: '100%',
+    height: '50%',
   },
 });
